413ecd7b08060fc7f57e84773310ad0b20478b4b
Ko <kdowling@unca.edu>
2023-10-18 22:47:00 -0400

    comcocomcomcomco

---
+++ lectures/lecture14/demo/greeting.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    document.querySelector("#message").innerHTML = "Hi hi!"
+    document.querySelector("#message").style.color= "red"
+}
+
+function sayGoodbye() {
+    document.querySelector("#message").innerHTML= "Bye!"
+    document.querySelector("#message").style.color= "bl7ue"
+}
